91e158f17aca005ed5f746380dfdff57ca16995f
Pranesh M <114741538+Pranesh-Murugesan@users.noreply.github.com>
2025-07-06 13:23:42 +0530

    Delete WEEK_3_SpringDataJPA directory

--- WEEK_3_SpringDataJPA/HandsOn_4_Hybernate_SpringDataJPA/code/SpringDataJPA/springdata-example/src/main/java/com/example/springdata/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springdata.repository;
-
-import com.example.springdata.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
